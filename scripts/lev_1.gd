618eb16ddc4e83364472fd34f055144516c732dc
Francesco <fra.muriana@gmail.com>
2026-05-22 14:22:43 +0200

    Refactor lev_1: Use unified level template

--- scripts/lev_1.gd
+++ scripts/lev_1.gd
@@ -1,24 +1,18 @@
-extends "res://scripts/base_level.gd"
+extends "res://scripts/level_template.gd"
 
-func _ready():
-	grid_step = 100.0
-	grid_offset_x = 0.0
-	grid_offset_y = 0.0
-
-	var sagoma = Polygon2D.new()
-	sagoma.color = Color(0, 0, 0, 0.3)
-	sagoma.polygon = PackedVector2Array([
+func setup_level():
+	grid_config = {
+		"grid_step": 100.0,
+		"grid_offset_x": 0.0,
+		"grid_offset_y": 0.0
+	}
+	piece_scale = 1.0
+	
+	create_sagoma(PackedVector2Array([
 		Vector2(400, 200), Vector2(800, 200), Vector2(800, 400),
 		Vector2(600, 400), Vector2(600, 600), Vector2(400, 600)
-	])
-	add_child(sagoma)
-
+	]))
+	
 	spawn_quadrato("Pezzo_Rosso", Vector2(200, 200))
 	spawn_quadrato("Pezzo_Verde", Vector2(1000, 200))
 	spawn_quadrato("Pezzo_Blu", Vector2(200, 500))
-
-func spawn_quadrato(nome, pos):
-	var p = preload("res://square.tscn").instantiate()
-	p.name = nome
-	p.position = pos
-	add_child(p)
