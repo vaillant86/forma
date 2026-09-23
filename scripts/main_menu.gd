cad0aefa674a8f09fb7363147a652320c0748207
Francesco <fra.muriana@gmail.com>
2026-09-23 14:51:34 +0200

    Improve main menu layout and usability

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,38 +1,51 @@
 extends Control
 
 func _ready():
-	if SaveManager.current_level == 1 and SaveManager.save_data["livello_sbloccato"] > 1:
-		SaveManager.current_level = SaveManager.save_data["livello_sbloccato"]
+    _update_continue_button()
+    _update_audio_button()
+    _focus_first_button()
 
-	if SaveManager.save_data.has("livello_sbloccato") and SaveManager.save_data["livello_sbloccato"] > 1:
-		$VBoxContainer/BtnContinua.disabled = false
-		$VBoxContainer/BtnContinua.text = "CONTINUE (Level " + str(SaveManager.current_level) + ")"
-	else:
-		$VBoxContainer/BtnContinua.disabled = true
-		$VBoxContainer/BtnContinua.text = "CONTINUE"
+func _focus_first_button():
+    var first_button = $CenterContainer/VBoxContainer/BtnNuovaPartita
+    if is_instance_valid(first_button):
+        first_button.grab_focus()
+
+func _update_continue_button():
+    if SaveManager.current_level == 1 and SaveManager.save_data.get("livello_sbloccato", 1) > 1:
+        SaveManager.current_level = SaveManager.save_data["livello_sbloccato"]
+
+    var continue_btn = $CenterContainer/VBoxContainer/BtnContinua
+    if SaveManager.save_data.has("livello_sbloccato") and SaveManager.save_data["livello_sbloccato"] > 1:
+        continue_btn.disabled = false
+        continue_btn.text = "CONTINUE (Level " + str(SaveManager.current_level) + ")"
+    else:
+        continue_btn.disabled = true
+        continue_btn.text = "CONTINUE"
+
+func _update_audio_button():
+    var audio_button = $MarginContainer/BtnAudio
+    if MusicManager.is_muted:
+        audio_button.text = "AUDIO: OFF"
+    else:
+        audio_button.text = "AUDIO: ON"
 
 func _on_btn_nuova_partita_pressed():
-	SaveManager.save_data["livello_sbloccato"] = 1
-	SaveManager.current_level = 1
-	SaveManager.save_game()
-	get_tree().change_scene_to_file("res://levels/lev_01.tscn")
+    SaveManager.save_data["livello_sbloccato"] = 1
+    SaveManager.current_level = 1
+    SaveManager.save_game()
+    get_tree().change_scene_to_file("res://levels/lev_01.tscn")
 
 func _on_btn_continua_pressed():
-	var level_str = "%02d" % SaveManager.current_level
-	var path = "res://levels/lev_" + level_str + ".tscn"
-	if ResourceLoader.exists(path):
-		get_tree().change_scene_to_file(path)
-	else:
-		push_error("Impossibile trovare la scena al percorso: " + path)
+    var level_str = "%02d" % SaveManager.current_level
+    var path = "res://levels/lev_" + level_str + ".tscn"
+    if ResourceLoader.exists(path):
+        get_tree().change_scene_to_file(path)
+    else:
+        push_error("Impossibile trovare la scena al percorso: " + path)
 
 func _on_btn_esci_pressed():
-	get_tree().quit()
-
-func _process(delta):
-	if MusicManager.is_muted:
-		$BtnAudio.text = "AUDIO: OFF"
-	else:
-		$BtnAudio.text = "AUDIO: ON"
+    get_tree().quit()
 
 func _on_btn_audio_pressed():
-	MusicManager.toggle_mute()
+    MusicManager.toggle_mute()
+    _update_audio_button()
